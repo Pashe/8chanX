1462fc4af493d31d1163d9c646b4e05bd4c75742
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-09-22 01:19:46 +0200

    Use custom auto-update, auto-update stats

--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
-// @name        Tux3's cheap 8chan userscript
-// @version     1.0
+// @name        Tux3's cheap 8chan-X userscript
+// @version     1.1
 // @namespace   8chan-X
-// @description Small userscript to improve 8chan
+// @description Userscript for productive 8chan browsing
 // @match       *://8chan.co/*
 // @run-at      document-end
 // @grant       none
 // ==/UserScript==
 
+/**************************************
+MENU BAR
+**************************************/
 function getStyleName() {
   var matches = document.URL.match(/\/(\w+)\/($|res\/\d+\.html|index\.html|res\/\d+\+50\.html)/);
   var board_name;
@@ -41,6 +44,13 @@ function updateMenuStyle() {
     menu.style.backgroundColor = "#333";
 }
 
+function updateMenuStats() {
+  var stats = document.getElementById("menuStats");
+  var nPosts = document.getElementsByClassName("post reply").length+1;
+  var nImages = document.getElementsByClassName("post-image").length;
+  stats.innerHTML = "["+nPosts+" / "+nImages+"]";
+}
+
 window.onload = function() {
   // Customize the menu
   var menu = document.getElementsByClassName("boardlist")[0];
@@ -60,6 +70,7 @@ window.onload = function() {
   var statsNode=document.createElement("SPAN");
   var statsTextNode=document.createTextNode("["+nPosts+" / "+nImages+"]");
   statsNode.appendChild(statsTextNode);
+  statsNode.id = 'menuStats';
   menu.appendChild(statsNode);
   
   // Hook style changes to update the menu's style
@@ -73,6 +84,10 @@ window.onload = function() {
     
 }
 
+
+/***************************************
+KEYBOARD EVENTS
+***************************************/
 document.addEventListener('keydown', function(event) {
   var activeElem = document.activeElement;
   
@@ -84,4 +99,101 @@ document.addEventListener('keydown', function(event) {
   if (event.keyCode === event.DOM_VK_R) {
       document.location.reload(); 
   }
+});
+
+
+/*******************************************
+AUTO UPDATES
+*******************************************/
+// Try to disable the official auto updater (bad hack but I can't see anything better)
+var highestTimeoutId = setTimeout("clearAllTimers");
+for (var i = 0 ; i < highestTimeoutId ; i++) {
+    clearTimeout(i); 
+}
+$(window).off('scroll');
+
+// Then implement our own patched auto-updater
+auto_reload_enabled = true;
+$(document) .ready(function () {
+  if ($('div.banner') .length == 0)
+  return ;
+  if ($('.post.op') .size() != 1)
+  return ;
+  var poll_interval;
+  var settings = new script_settings('auto-reload');
+  var poll_interval_mindelay_bottom = settings.get('min_delay_bottom', 3000);
+  var poll_interval_mindelay_top = settings.get('min_delay_top', 10000);
+  var poll_interval_maxdelay = settings.get('max_delay', 600000);
+  var poll_interval_shortdelay = settings.get('quick_delay', 100);
+  var poll_interval_delay = poll_interval_mindelay_bottom;
+  var end_of_page = false;
+  var new_posts = 0;
+  var first_new_post = null;
+  if (typeof update_title == 'undefined') {
+    var update_title = function () {
+    };
+  }
+  if (typeof add_title_collector != 'undefined')
+  add_title_collector(function () {
+    return new_posts;
+  });
+  var window_active = true;
+  $(window) .focus(function () {
+    window_active = true;
+    recheck_activated();
+    if (settings.get('reset_focus', true)) {
+      poll_interval_delay = end_of_page ? poll_interval_mindelay_bottom : poll_interval_mindelay_top;
+    }
+  });
+  $(window) .blur(function () {
+    window_active = false;
+  });
+  var recheck_activated = function () {
+    if (new_posts && window_active && $(window) .scrollTop() + $(window) .height() >= $(first_new_post) .position() .top) {
+      new_posts = 0;
+    }
+    update_title();
+  };
+  var poll = function () {
+    $.ajax({
+      url: document.location,
+      success: function (data) {
+        $(data) .find('div.post.reply') .each(function () {
+          var id = $(this) .attr('id');
+          if ($('#' + id) .length == 0) {
+            if (!new_posts) {
+              first_new_post = this;
+            }
+            $(this) .insertAfter($('div.post:last') .next()) .after('<br class="clear">');
+            new_posts++;
+            $(document) .trigger('new_post', this);
+            recheck_activated();
+          }
+        });
+        time_loaded = Date.now();
+        updateMenuStats();
+      }
+    });
+    clearTimeout(poll_interval);
+    if (new_posts == 0) {
+      poll_interval_delay *= 2;
+      if (poll_interval_delay > poll_interval_maxdelay) {
+        poll_interval_delay = poll_interval_maxdelay;
+      }
+    } else {
+      poll_interval_delay = end_of_page ? poll_interval_mindelay_bottom : poll_interval_mindelay_top;
+    }
+    poll_interval = setTimeout(poll, poll_interval_delay);
+  };
+  $(window) .scroll(function () {
+    recheck_activated();
+    if ($(this) .scrollTop() + $(this) .height() < $('div.post:last') .position() .top + $('div.post:last') .height()) {
+      end_of_page = false;
+      return ;
+    }
+    clearTimeout(poll_interval);
+    poll_interval = setTimeout(poll, poll_interval_shortdelay);
+    end_of_page = true;
+  }) .trigger('scroll');
+  poll_interval = setTimeout(poll, poll_interval_delay);
 });
