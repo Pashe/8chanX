8d7df969554d9ad85dbf46c498e25ddc78838c05
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-09-22 20:33:58 +0200

    Enable auto-updates

---
+++ 8chan-x.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Tux3's 8chan X
+// @version     1.3
+// @namespace   8chan-X
+// @description Small userscript to improve 8chan
+// @match       *://8chan.co/*
+// @run-at      document-end
+// @grant       none
+// @updateURL 	 https://github.com/tux3/8chan-X/raw/master/8chan-x.meta.js
+// @downloadURL  https://github.com/tux3/8chan-X/raw/master/8chan-x.user.js
+// ==/UserScript==
+
--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name        Tux3's cheap 8chan userscript
+// @name        Tux3's 8chan X
 // @version     1.3
 // @namespace   8chan-X
 // @description Small userscript to improve 8chan
 // @match       *://8chan.co/*
 // @run-at      document-end
 // @grant       none
+// @updateURL 	 https://github.com/tux3/8chan-X/raw/master/8chan-x.meta.js
+// @downloadURL  https://github.com/tux3/8chan-X/raw/master/8chan-x.user.js
 // ==/UserScript==
 
 /*********
