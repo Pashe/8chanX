b496e6643978991739853d57a4effd7360c7b95c
Pashe <pashe@users.noreply.github.com>
2014-10-09 15:54:14 -0500

    1.35.5

--- 8chan-x.meta.js
+++ 8chan-x.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Pashe's 8chanX
-// @version     1.35.4
+// @version     1.35.5
 // @namespace   https://github.com/Pashe/
 // @description Small userscript to improve 8chan
 // @match       *://8chan.co/*
