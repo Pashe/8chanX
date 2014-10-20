4b6fcf4ef26301864288e7a78630fa41a2e15653
Pashe <pashe@users.noreply.github.com>
2014-10-19 20:31:11 -0500

    Remove some probably-extra stuff from the metafile

--- 8chan-x.meta.js
+++ 8chan-x.meta.js
@@ -1,12 +1,8 @@
 // ==UserScript==
 // @name        Pashe's 8chanX
-// @version     1.35.9.1413768250
+// @version     1.35.9.1413768670
 // @namespace   https://github.com/Pashe/
 // @description Small userscript to improve 8chan
-// @match       *://8chan.co/*
-// @run-at      document-end
-// @grant       none
-// @require     http://timeago.yarp.com/jquery.timeago.js
 // @updateURL   https://github.com/Pashe/8chan-X/raw/master/8chan-x.meta.js
 // @downloadURL https://github.com/Pashe/8chan-X/raw/master/8chan-x.user.js
 // ==/UserScript==
