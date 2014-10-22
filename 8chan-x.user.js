08124ef9fb61332ab0925935e1d61d8ddef1139b
Pashe <pashe@users.noreply.github.com>
2014-10-22 16:14:28 -0500

    Move addCatalogNullImagePlaceholders up one place in initCatalog

--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Pashe's 8chanX
-// @version     1.35.9.1413962450
+// @version     1.35.9.1414012460
 // @namespace   https://github.com/Pashe/
 // @description Small userscript to improve 8chan
 // @match       *://8chan.co/*
@@ -933,8 +933,8 @@ function initCatalog() {
 		addCatalogPages();
 		setCatalogTitle();
 		highlightCatalogAutosage();
-		setCatalogImageSize();
 		addCatalogNullImagePlaceholders();
+		setCatalogImageSize();
 	}
 }
 
