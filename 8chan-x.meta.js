7aab76ed1dd888df53992b3c2342ad855b066eff
Pashe <pashe@users.noreply.github.com>
2014-11-05 21:24:13 -0600

    Remove QR keyboard shortcuts

--- 8chan-x.meta.js
+++ 8chan-x.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Pashe's 8chanX
-// @version     1.35.9.1414319450
+// @version     1.35.9.1415244250
 // @namespace   https://github.com/Pashe/
 // @description Small userscript to improve 8chan
 // @updateURL   https://github.com/Pashe/8chan-X/raw/master/8chan-x.meta.js
