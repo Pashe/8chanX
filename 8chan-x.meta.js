11e6efea49ca9ff074e556f3be3e1ff0fc7ca4e6
Pashe <pashe@users.noreply.github.com>
2014-12-27 23:08:43 -0600

    I am still bad at git

--- 8chan-x.meta.js
+++ 8chan-x.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name        Pashe's 8chanX v2
-// @version     2.0.0.1419469610
+// @name        Pashe's 8chanX v2 [pure]
+// @version     2.0.0.1419743790
 // @description Small userscript to improve 8chan
 // @namespace   https://github.com/Pashe/8chan-X/tree/2-0
 // @updateURL   https://github.com/Pashe/8chan-X/raw/2-0/8chan-x.meta.js
