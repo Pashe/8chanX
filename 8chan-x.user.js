cf58a5ee0fbb6905b5abfeca6535b21f2efead14
Pashe <pashe@users.noreply.github.com>
2015-01-01 22:36:08 -0600

    WIP gallery

--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Pashe's 8chanX v2 [pure]
-// @version     2.0.0.1419993590
+// @version     2.0.0.1420173360
 // @description Small userscript to improve 8chan
 // @icon        https://github.com/Pashe/8chan-X/raw/2-0/images/logo.svg
 // @namespace   https://github.com/Pashe/8chan-X/tree/2-0
@@ -37,6 +37,7 @@ var bumpLimit = 300;
 
 //Initializations
 var cachedPages = null;
+var galleryImages;
 
 //Dynamic
 var originalPageTitle = window.document.title;
@@ -440,6 +441,23 @@ function notifyReplies() {
 ////////////////
 //INIT FUNCTIONS
 ////////////////
+
+////////////////
+//GALLERY
+////////////////
+function refreshGalleryImages() {
+	galleryImages = [];
+	
+	$(".post-image").each(function() {
+		if (!this.src.match(/\/deleted.png$/)) {
+			galleryImages.push({
+				"thumbnail": this.src,
+				"full":      this.parentNode.href
+			})
+		}
+	})
+}
+
 function initSettings() {
 	refreshSettings();
 	var settingsItems = settingsMenu.getElementsByTagName("input");
@@ -580,6 +598,9 @@ function initKeyboardShortcuts() { //Pashe, heavily influenced by Tux et al, WTF
 				case 81:
 					showQR();
 					e.preventDefault();
+					break;
+				case 71:
+					initGallery();
 					break;
 				case 69:
 					toggleExpandAll();
@@ -787,6 +808,48 @@ function initFlagIcons() { //Anon from >>>/tech/60489, presumably WTFPL or simil
 			opt.style.paddingLeft = '20px';
 			if (opt.value)
 					opt.style.background = 'no-repeat left center url(' + custom_flag_url + opt.value + '.png)';
+	}
+}
+
+function initGallery() {
+	refreshGalleryImages();
+	
+	var galleryHolder = $("<div id='chx_gallery'></div>");
+	galleryHolder.appendTo($("body"));
+	
+	galleryHolder.css({
+		"background-color": "rgba(0,0,0,0.7)",
+		"z-index":          "101",
+		"position":         "fixed",
+		"left":             "0",
+		"top":              "0",
+		"width":            "100%",
+		"height":           "100%"
+	});
+	
+	for (i in galleryImages) {
+		var thumbHolder = $('<div class="chx_galleryThumbHolder"></div>')
+		var thumbLink = $(sprintf('<a class="chx_galleryThumbLink" href="%s"></a>', galleryImages[i]["full"]));
+		var thumbImage = $(sprintf('<img class="chx_galleryThumbImage" src="%s" />', galleryImages[i]["thumbnail"]));
+		
+		thumbImage.css({
+			"max-height": "100%",
+			"max-width":  "100%",
+			"margin":     "auto",
+			"display":    "block"
+		});
+		
+		thumbHolder.css({
+			"border":    "2pt solid #888",
+			"height":    "128px",
+			"width":     "128px",
+			"overflow":  "hidden",
+			"float":     "left"
+		});
+		
+		thumbImage.appendTo(thumbLink);
+		thumbLink.appendTo(thumbHolder);
+		thumbHolder.appendTo(galleryHolder);
 	}
 }
 
