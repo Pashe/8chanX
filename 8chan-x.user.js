1438b8e8b9c5e7b07bdbebb83d77bf6eecb81c73
Pashe <pashe@users.noreply.github.com>
2015-01-30 19:41:55 -0600

    Hide cursor while hovering

--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Pashe's 8chanX v2
-// @version     2.0.0.1422605590
+// @version     2.0.0.1422668510
 // @description Small userscript to improve 8chan
 // @icon        https://cdn.rawgit.com/Pashe/8chanX/2-0/images/logo.svg
 // @namespace   https://github.com/Pashe/8chanX/tree/2-0
@@ -359,6 +359,7 @@ function imageHoverStart(e) { //Pashe, WTFPL
 	});
 	
 	hoverImage.appendTo($("body"));
+	$this.css("cursor", "none");
 }
 
 function imageHoverEnd() { //Pashe, WTFPL
