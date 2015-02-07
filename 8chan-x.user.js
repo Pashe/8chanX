7b044da9496552887063e6eef3ac2cad2da22275
Pashe <pashe@users.noreply.github.com>
2015-02-07 12:58:30 -0600

    Progress cursor while waiting for catalog image hover

--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Pashe's 8chanX v2
-// @version     2.0.0.1423102040
+// @version     2.0.0.1423336140
 // @description Small userscript to improve 8chan
 // @icon        https://cdn.rawgit.com/Pashe/8chanX/2-0/images/logo.svg
 // @namespace   https://github.com/Pashe/8chanX/tree/2-0
@@ -368,12 +368,14 @@ function imageHoverStart(e) { //Pashe, anonish, WTFPL
 	if ($this.parent().attr("href").match("src")) {
 		fullUrl = $this.parent().attr("href");
 	} else if (isOnCatalog()) {
+		$this.css("cursor", "progress");
 		fullUrl = $this.attr("src");
 		$.ajax(($this.parent().attr("href").replace(/\.html$/, ".json")), {
 			success: function (result) {
 				fullUrl = result.posts[0].tim + result.posts[0].ext;
 				if (isVideo(getFileExtension(fullUrl))) {return;}
 				$("#chx_hoverImage").attr("src", sprintf("/%s/src/%s", thisBoard, fullUrl));
+				$("#chx_hoverImage").on("load", function() {$this.css("cursor", "none")});
 			},
 			async: true,
 			cache: true
