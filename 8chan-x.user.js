bd68bb8fdf42bb1fafa2c566d86b6c9f704dadac
Pashe <pashe@users.noreply.github.com>
2015-02-07 18:36:26 -0600

    I hate pure so much (fixes #14)

--- 8chan-x.user.js
+++ 8chan-x.user.js
@@ -1,7 +1,6 @@
 // ==UserScript==
-<<<<<<< HEAD
 // @name        Pashe's 8chanX v2 [pure]
-// @version     2.0.0.1423336230
+// @version     2.0.0.1423355780
 // @description Small userscript to improve 8chan
 // @icon        https://cdn.rawgit.com/Pashe/8chanX/2-0_pure/images/logo.svg
 // @namespace   https://github.com/Pashe/8chanX/tree/2-0
